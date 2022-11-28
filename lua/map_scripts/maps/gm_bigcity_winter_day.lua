cb21cea46d19b413db91760c236fe680d6f89361
Redox <mail@redox.to>
2022-11-28 23:45:50 +0100

    Update lua/map_scripts/maps/gm_bigcity_winter_day.lua
    Co-authored-by: Brandon Sturgeon <brandon@brandonsturgeon.com>

--- lua/map_scripts/maps/gm_bigcity_winter_day.lua
+++ lua/map_scripts/maps/gm_bigcity_winter_day.lua
@@ -3,6 +3,8 @@ local scareButtonNumber = 3342
 -- Removes the mapwide jumpscare sound
 hook.Add( "InitPostEntity", "CFC_MapScripts_bigcitywinter", function()
     local ent = ents.GetMapCreatedEntity( scareButtonNumber )
-    if not IsValid( ent ) then return end
+    if not IsValid( ent ) then
+        return ErrorNoHaltWithStack( "Couldn't find jumpscare button!" )
+    end
     ent:Remove()
 end )
