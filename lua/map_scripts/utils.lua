463d42ebb234d7b0df29e06633b2468072db6263
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2024-10-13 14:02:18 -0700

    Move to init

--- lua/map_scripts/utils.lua
+++ lua/map_scripts/utils.lua
@@ -27,11 +27,3 @@ function Utils.DisableShadowControl()
         shadowControl:Fire( "SetShadowsDisabled", 1 )
     end
 end
-
-hook.Add( "InitPostEntity", "CFC_MapScripts_PostMapEntsSpawn", function()
-    hook.Run( "CFC_MapScripts_PostMapEntsSpawn" )
-end )
-
-hook.Add( "PostCleanupMap", "CFC_MapScripts_PostMapEntsSpawn", function()
-    hook.Run( "CFC_MapScripts_PostMapEntsSpawn" )
-end )
