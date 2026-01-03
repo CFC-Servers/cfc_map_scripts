6a42aca8299698ebb76f9e2241914f6a50fb6352
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2026-01-02 18:52:48 -0700

    Error if removing unexpected ents

--- lua/map_scripts/maps/gm_vacant_industry_revamped.lua
+++ lua/map_scripts/maps/gm_vacant_industry_revamped.lua
@@ -10,11 +10,20 @@ local entIds = {
     2718, -- fast
 }
 
+local buttonClass = "func_button"
+
 hook.Add( "CFC_MapScripts_PostMapEntsSpawn", "CFC_MapScripts_vacant_removeadminbuttons", function()
     local removedEnts = 0
     for _, entId in ipairs( entIds ) do
         local ent = ents.GetMapCreatedEntity( entId )
-        if not IsValid( ent ) then continue end
+        if not IsValid( ent ) then
+            ErrorNoHaltWithStack("Admin button removal: Couldn't find " .. entId)
+            continue
+        end
+        if ent:GetClass() ~= buttonClass then
+            ErrorNoHaltWithStack("Admin button removal: Entity " .. entId .. " is not a button!")
+            continue
+        end
         removedEnts = removedEnts + 1
         SafeRemoveEntity( ent )
     end
