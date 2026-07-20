d2a216b6b36a930e13b9c60bc226f330105d7ddc
Pierce Lally <pierce@vulpes.dev>
2026-07-19 21:15:42 -0400

    Rename hook for blocking workshop tracks

--- lua/map_scripts/maps/gm_sunsetgulch.lua
+++ lua/map_scripts/maps/gm_sunsetgulch.lua
@@ -1,7 +1,7 @@
 resource.AddWorkshop( "311697867" ) -- map
 local tracksWSID = "391016040"
 resource.AddWorkshop( tracksWSID ) -- tracks
-hook.Add( "Contentalizer_ShouldAddWorkshop", "BlockSpecificAddon", function( addon )
+hook.Add( "Contentalizer_ShouldAddWorkshop", "BlockTrackThingForSunsetGulch", function( addon )
     if tostring( addon.wsid ) == tracksWSID then
         return false
     end
